a1e618ecb750be1f5caa95db61904a46c954af98
lorena-iordache <116649455+lorena-iordache@users.noreply.github.com>
2022-10-25 15:55:34 +0300

    Initial commit
    Initial commit

---
+++ pages/profesori/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <header class="blog header">
+      <div class="foreground">
+        <div class="page-bar wrapper">
+          <Navigation></Navigation>
+        </div>
+      </div>
+    </header>
+
+    <section class="body-container">
+      <div class="items-bar wrapper">
+        <h2>Articles for <b>profesori</b> ({{ posts.length }})</h2>
+      </div>
+      <ul class="items-list wrapper">
+        <li class="item" v-for="post in posts">
+          <article-preview :post="post"></article-preview>
+        </li>
+      </ul>
+    </section>
+
+  </div>
+</template>
+
+<script>
+import {createClient} from '~/plugins/contentful.js'
+import Navigation from '~/components/navigation.vue'
+import ArticlePreview from '~/components/article-preview.vue'
+
+const client = createClient()
+
+export default {
+  asyncData ({ env, params }) {
+    return client.getEntries({
+      'content_type': env.CTF_BLOG_POST_TYPE_ID,
+      order: '-sys.createdAt',
+      'fields.tags[in]': 'profesori'
+    }).then(entries => {
+      console.log(entries)
+      return {
+        posts: entries.items
+      }
+    })
+  },
+  components: {
+    ArticlePreview,
+    Navigation
+  }
+}
+</script>
